66a89ff0b6cbcc12e043b00c0da89f601a997c43
Mauricio Gamboa C <Mauricio Gamboa C@172.26.96.115>
2015-05-25 14:24:19 -0600

    Cambios sobre Main

--- MemoriaVirtualPrubasMP/src/memoriavirtualprubasmp/Main.java
+++ MemoriaVirtualPrubasMP/src/memoriavirtualprubasmp/Main.java
@@ -5,6 +5,7 @@
  */
 package memoriavirtualprubasmp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -17,18 +18,21 @@ public class Main {
     public static List<String> lineas_archivos_procesos;
     public static List<String> lineas_archivos_referencias;
     public static List<Proceso> lista_Procesos;
-    public static Frame[][] memoria_fisica = new Frame[5][5];
-    public static Frame[][] memoria_virtual = new Frame[6][5];
+    
+    public static List<Frame> memoria_fisica = new LinkedList<Frame>();
+    public static List<Frame>memoria_virtual = new LinkedList<Frame>();; 
+    
     public static int tamaño_fijo; 
     
     public static void main(String[] args) {
         Proceso p1 = new Proceso("P1",2,50);
         Proceso p2 = new Proceso("P2",2,50);
         Proceso p3 = new Proceso("P3",2,70);
+        
         Frame f1 = new Frame (1,p1,10); 
         Frame f2 = new Frame (2,p1,10); 
         Frame f3 = new Frame (3,p1,10); 
-        Frame f4 = new Frame (3,p1,10); 
+        Frame f4 = new Frame (4,p1,10); 
         Frame f5 = new Frame (1,p2,10); 
         Frame f6 = new Frame (1,p3,10); 
         Frame f7 = new Frame (2,p3,10); 
@@ -37,30 +41,36 @@ public class Main {
         Frame f10 = new Frame (5,p3,10); 
         Frame f11 = new Frame (6,p3,10); 
      
-        memoria_virtual[0][0]=f1; 
-        memoria_virtual[0][1]=f2;
-        memoria_virtual[0][2]=f3;
-        memoria_virtual[0][3]=f4;
-        memoria_virtual[0][4]=f5;
+        memoria_virtual.add(f1); 
+        memoria_virtual.add(f2);
+        memoria_virtual.add(f3);
+        memoria_virtual.add(f4);
+        memoria_virtual.add(f5);
         
-        memoria_virtual[1][0]=f6; 
-        memoria_virtual[1][1]=f7;
-        memoria_virtual[1][2]=f8;
-        memoria_virtual[1][3]=f9;
-        memoria_virtual[1][4]=f10;
+        memoria_virtual.add(f6); 
+        memoria_virtual.add(f7);
+        memoria_virtual.add(f8);
+        memoria_virtual.add(f9);
+        memoria_virtual.add(f10);
         
-        memoria_virtual[2][0]=f11; 
+        memoria_virtual.add(f11); 
         
         ResidentSet res = new ResidentSet(); 
-        res.ResidentSetFijo();
-        System.out.println(memoria_fisica[0][0]); 
-        System.out.println(memoria_fisica[0][1]); 
-        System.out.println(memoria_fisica[0][2]); 
-        System.out.println(memoria_fisica[0][3]); 
-        System.out.println(memoria_fisica[0][4]); 
-        System.out.println(memoria_fisica[1][0]); 
-        System.out.println(memoria_fisica[1][1]); 
-        System.out.println(memoria_fisica[1][2]); 
+        res.ResidentSetFijo(2);
+        
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println(memoria_fisica.get(0).contenido.nombre); 
+        System.out.println(memoria_fisica.get(1).contenido.nombre); 
+        
+        System.out.println(memoria_fisica.get(2).contenido.nombre); 
+        System.out.println("Reservado"); 
+        
+        //System.out.println(memoria_fisica.get(3).contenido.nombre); 
+        System.out.println(memoria_fisica.get(4).contenido.nombre); 
+        System.out.println(memoria_fisica.get(5).contenido.nombre); 
+        
+        //System.out.println(memoria_fisica.get(6).contenido.nombre); 
+        //System.out.println(memoria_fisica.get(7).contenido.nombre); 
     }
     
 }
--- MemoriaVirtualPrubasMP/src/memoriavirtualprubasmp/ResidentSet.java
+++ MemoriaVirtualPrubasMP/src/memoriavirtualprubasmp/ResidentSet.java
@@ -12,44 +12,54 @@ package memoriavirtualprubasmp;
 public class ResidentSet {
     
     
-    public void ResidentSetFijo() {
-        //Proceso p = new Proceso("P1",5, 5);
-        //Frame j = new Frame(5,p ,25);
-        //Frame[][] frame1[0][1] = j;  
-        int columnas_memoria_virtual = 5; 
-        int filas_memoria_virtual =5; 
+    public void ResidentSetFijo(int tamano_fijo) {  
+        //Validar que no se sobrepase la cantidad de frames en memoria física
         int contador=0; 
-        int cantidad_maxima_frames = Main.tamaño_fijo;
-        for (int i = 0; i < columnas_memoria_virtual; i ++ ) {
-            for (int j = 0; j < filas_memoria_virtual; j ++ ) {
-                String nombre_proceso = Main.memoria_virtual[0][0].contenido.nombre; 
-                // Initialize each object
-                int columnas_memoria_fisica = 5; 
-                int filas_memoria_fisica =5; 
-                for (int k = 0; k < columnas_memoria_fisica; k ++ ) {
-                    for (int l = 0; l < filas_memoria_fisica; l ++ ) {
-                        if ((contador < cantidad_maxima_frames) & (Main.memoria_virtual[i][j].contenido.nombre.equalsIgnoreCase(nombre_proceso))){
-                            Main.memoria_fisica[k][l]=Main.memoria_virtual[i][j]; 
-                            contador++; 
-                        }
-                        else if ((contador < cantidad_maxima_frames) & (!(Main.memoria_virtual[i][j].contenido.nombre.equalsIgnoreCase(nombre_proceso)))){
-                            // no hace nada y el for se brinca la posición 
-                        }
-                        else{
-                            if (!(Main.memoria_virtual[i][j].contenido.nombre.equalsIgnoreCase(nombre_proceso))){
-                                nombre_proceso=Main.memoria_virtual[i][j].contenido.nombre; 
-                                contador=0; 
-                            }
-                             
-                        }
-                    }
-                }
+        int cantidad_maxima_frames = tamano_fijo;
+        String nombre_proceso = Main.memoria_virtual.get(0).contenido.nombre; 
+        //System.out.println("Nombre Primer Proceso: "+nombre_proceso);
+        int ubicacion_fisica=0; 
+        for (int i = 0; i < Main.memoria_virtual.size(); i++ ) {
+            // Initialize each object
+            if ((contador < cantidad_maxima_frames) && (Main.memoria_virtual.get(i).contenido.nombre.equalsIgnoreCase(nombre_proceso))){
+                //System.out.println("Primer If, Nombre proceso: "+nombre_proceso+" - Contador: "+contador);
+                Main.memoria_fisica.add(ubicacion_fisica,Main.memoria_virtual.get(i)); 
+                //System.out.println("Primer If, Cambio Memoria Fisica, Proceso Actual: "+nombre_proceso+" - Nombre Cambiado: "+Main.memoria_fisica.get(ubicacion_fisica).contenido.nombre);
+                ubicacion_fisica++; 
+                contador++; 
             }
+            else if ((contador < cantidad_maxima_frames) & (!(Main.memoria_virtual.get(i).contenido.nombre.equalsIgnoreCase(nombre_proceso)))){ 
+                Main.memoria_fisica.add(ubicacion_fisica,null); 
+                contador++; 
+                ubicacion_fisica++; 
+                //System.out.println("Segundo If, Nombre proceso espacio reservado: "+nombre_proceso+" - Contador: "+contador);   
+            }
+            else{
+                if (!(Main.memoria_virtual.get(i).contenido.nombre.equalsIgnoreCase(nombre_proceso))){
+                    nombre_proceso=Main.memoria_virtual.get(i).contenido.nombre; 
+                    Main.memoria_fisica.add(ubicacion_fisica,Main.memoria_virtual.get(i));
+                    contador=1; 
+                    ubicacion_fisica++; 
+                }
+            }              
         }
-    
-    
     }
     
+    
+    
+    public void ResidentSetVariable (String nombre_proceso){
+        int tamano_inicial=2; 
+        int tamano_maximo=5; 
+        int tamano_crecimiento=2; 
+        //Se ocupa el next o el first ??
+        
+        for (int i = 0; i < Main.memoria_virtual.size(); i++ ) {
+            
+        }
+        
+    }
+    
+  
     
     
     
